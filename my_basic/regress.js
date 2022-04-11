3e3bec370ccd056046a30c2ac546f336cb8ba3d6
ChangRui <critiswhatitis@gmail.com>
2022-04-11 16:58:47 +0800

    fix:jenkings build

--- my_basic/regress.js
+++ my_basic/regress.js
@@ -3,10 +3,13 @@ var url="www.baidu.com?a=1&b=2&C=3";//测试地址
 		分析：
 			最前面是?或&，紧跟着除 ?&#以外的字符若干，然后再等号，最后再跟着除 ?&#以外的字符，并且要分组捕获到【除?&#以外的字符】
 	*/
-	var reg=/[?&]([^?&#]+)=([^?&#]+)/g;
 	
+	var reg=/[?&]([^?&#]+)=([^?&#]+)/g;
+	var reg2=/([^?&#]+)=([^?&#]+)/g;
 	var param={};
 	var ret =  reg.exec(url);
+    let res=url.match(reg2)
+     console.log(res)
 	while(ret){//当ret为null时表示已经匹配到最后了，直接跳出
 		param[ret[1]]=ret[2];
 		ret = reg.exec(url);
--- my_vite/src/components/Father.vue
+++ my_vite/src/components/Father.vue
@@ -9,10 +9,10 @@
 
 <script setup lang="ts">
 
-import { getCurrentInstance,ref,ComponentInternalInstance,useAttrs} from '@vue/runtime-core';
+import { ref,useAttrs} from '@vue/runtime-core';
 import GrandSon from './GrandSon.vue';
 // 利用ref获取自组建的东西很奇怪 为什么非要在函数里才能接收到？
-const {ctx} = getCurrentInstance()as ComponentInternalInstance
+// const {ctx} = getCurrentInstance()as ComponentInternalInstance
 const jojocall=ref('')
 // const jojocall2=function accept(){ 
 //   return currentInstance.ctx.$refs.child.call_father}
@@ -22,7 +22,7 @@ console.log(att)
 
 function show() {
   
-    jojocall.value=ctx?.$refs.child.call_father
+    // jojocall.value=ctx?.$refs.child.call_father
     console.log(jojocall)
 }
 const callmyson=(message:string)=>{
